f352ffe2934bf410fe35e0aa05cf04722fa18785
Kim Juho <sv05159@naver.com>
2021-10-19 00:42:35 +0900

    singleton
    - 인스턴스는 하나만 생성 되어야 한다. (private 생성자)
    - 생성된 하나의 인스턴스에 글로벌하게 접근 가능해야 한다. (static 메서드)
    => 대부분의 애플리케이션은 멀티 스레드로 동작하는데, 이 방법은 안전하지 않음.

---
+++ src/main/java/whiteship/designpatters/singleton/App.java
@@ -0,0 +1,11 @@
+package whiteship.designpatters.singleton;
+
+public class App {
+
+    public static void main(String[] args) {
+        Settings settings = Settings.getInstance();
+        Settings settings1 = Settings.getInstance();
+        System.out.println(settings == settings1);
+    }
+
+}
