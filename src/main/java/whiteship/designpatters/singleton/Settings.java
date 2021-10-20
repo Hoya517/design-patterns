14ff9ad3003f44d84b59ba6aa6a659fa466fa9d3
Kim Juho <sv05159@naver.com>
2021-10-21 01:08:58 +0900

    static inner 클래스 사용하기
    - 멀티 쓰레드 환경에서 안전
    - getInstance()가 호출될때 SettingsHolder 클래스가 로딩이되고 Settings를 생성하기 때문에 Lazy Loading도 가능해짐
    => 권장하는 방법 중 하나 (복잡한 이론적 배경과 코드가 많아지지 않음)

--- src/main/java/whiteship/designpatters/singleton/Settings.java
+++ src/main/java/whiteship/designpatters/singleton/Settings.java
@@ -2,20 +2,14 @@ package whiteship.designpatters.singleton;
 
 public class Settings {
 
-    private static volatile Settings instance;
-
     private Settings() {}
 
-    public static Settings getInstance() {
-        if (instance == null) {
-            synchronized (Settings.class) {  // 여기서만 동기화!
-                if (instance == null) {
-                    instance = new Settings();
-                }
-            }
-        }
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
 
-        return instance;
+    public static Settings getInstance() {
+        return SettingsHolder.INSTANCE;
     }
 
 }
